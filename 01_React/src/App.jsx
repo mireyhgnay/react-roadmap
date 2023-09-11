1c4fa932d08084bba5bccf7838312586a4eb0107
hyerim <hyerimiya1216@gmail.com>
2023-09-12 00:06:22 +0900

    Feat: props를 통해 컴포넌트에게 값 전달하기 스터디

--- 01_React/src/App.jsx
+++ 01_React/src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Hello from "./Hello";
+import Wrapper from "./Wrapper";
 
 function App() {
-  const name = "react";
+  const name = "변수로 전달한 name";
 
   // 인라인 스타일로 추가되는 경우
   const style = {
@@ -16,9 +17,13 @@ function App() {
     // 주석
     /* 주석주석 */
     <>
-      <Hello
-      // 열리는 태그 내부에서는 이렇게 주석을 작성 할 수 있습니다.
-      />
+      <Wrapper>
+        <Hello
+        // 열리는 태그 내부에서는 이렇게 주석을 작성 할 수 있습니다.
+        />
+        <Hello name="props로 전달한 name" color="red" />
+        <Hello color="pink" />
+      </Wrapper>
       <p style={style}>안녕하세요~!</p>
       <div className="name">{name}</div>
     </>
--- 01_React/src/Hello.js
+++ 01_React/src/Hello.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function Hello() {
-  return <div>안녕하세요</div>;
+// 비구조화 할당을 통한 props 설정
+export default function Hello({ color, name }) {
+  /**
+   * style={{ }} 중괄호 두번 사용하는 이유
+   * jsx 문법상 한번 감싸줘야 하고
+   * 그 안에 {} 중괄호는 객체리터럴을 사용하기 위한 중괄호 입니다.
+   */
+  return <h1 style={{ color }}>안녕하세요 {name}</h1>;
 }
+
+Hello.defaultProps = {
+  name: "이름없음",
+};
