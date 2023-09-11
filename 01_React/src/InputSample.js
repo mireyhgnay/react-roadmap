59d4fac426ce751d9c3eb0e667764ea4ee61179c
hyerim <hyerimiya1216@gmail.com>
2023-09-12 01:17:50 +0900

    Feat: 여러개의 input 상태 관리하기

--- 01_React/src/InputSample.js
+++ 01_React/src/InputSample.js
@@ -1,22 +1,48 @@
 import React, { useState } from "react";
 
 export default function InputSample() {
-  const [text, setText] = useState(0);
+  const [inputs, setInputs] = useState({
+    username: "",
+    nickname: "",
+  });
+
+  // 비구조화 할당을 통해 값 추출
+  const { username, nickname } = inputs;
 
   const onChange = (e) => {
-    setText(e.target.value);
+    const { value, name } = e.target;
+    // 가장 중요한 점은 리액트에서 객체를 업데이트하게 될 때에는 기존 객체를 직접 수정하면 안되고, 새로운 객체를 만들어서, 새 객체에 변화를 주어야 됩니다.
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value, // name 키를 가진 값을 value 로 설정
+    });
   };
 
   const onReset = () => {
-    setText("");
+    setInputs({
+      username: "",
+      nickname: "",
+    });
   };
 
   return (
     <div>
-      <input onChange={onChange} value={text} />
+      <input
+        name="username"
+        placeholder="이름"
+        onChange={onChange}
+        value={username}
+      />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {text}</b>
+        <b>값: </b>
+        {username} ({nickname})
       </div>
     </div>
   );
